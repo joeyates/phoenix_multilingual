5d9dd58534f9efa6bc4aad494b1b34fe44b6594a
Joe Yates <joe.g.yates@gmail.com>
2026-02-11 18:37:18 +0100

    Add missing test views

---
+++ test/support/project/lib/test_project_web/live/about_live.ex
@@ -0,0 +1,9 @@
+defmodule TestProjectWeb.AboutLive do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~H"""
+    <h1>About</h1>
+    """
+  end
+end
---
+++ test/support/project/lib/test_project_web/live/contacts_live.ex
@@ -0,0 +1,9 @@
+defmodule TestProjectWeb.ContactsLive do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~H"""
+    <h1>Contacts</h1>
+    """
+  end
+end
